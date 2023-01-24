80514d204ac48b27ff93ac9b7344de72a236a09a
HGM <horia.ghionoiu@alma-medical.com>
2023-01-24 23:45:59 +0000

    First commit.

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Hotspot Analysis Tool
+@jorialand - 24/01/2023
+Focus refactoring on what matters with Hotspots Analysis
+https://understandlegacycode.com/blog/focus-refactoring-with-hotspots-analysis/
+"""
+
+
+def computeComplexity():
+    pass
+
+
+def computeChurn():
+    pass
+
+
+def generateGraph():
+    pass
+
+
+if __name__ == "__main__":
+    computeComplexity()
+    computeChurn()
+    generateGraph()
